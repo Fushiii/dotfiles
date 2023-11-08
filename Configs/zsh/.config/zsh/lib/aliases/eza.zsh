cc7e5b716ebe4ea38e18f5dc0de5b17e59a5d8b6
enmeei <enmeeei@gmail.com>
2023-11-08 09:41:03 -0300

    feat: add zellij config

--- Configs/zsh/.config/zsh/lib/aliases/eza.zsh
+++ Configs/zsh/.config/zsh/lib/aliases/eza.zsh
@@ -2,10 +2,7 @@
 # They are used in all aliases for this.
 COMMON="--group-directories-first"
 
-
 alias l="exa '${COMMON}' -l"
 alias la="exa '${COMMON}' -la"
 alias ls="exa '${COMMON}'"
 alias lt="exa '${COMMON}' --tree"
-
-
